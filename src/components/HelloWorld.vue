0dc5353e934b209e42f70ee836d94533d79a3d21
Thomas Kirsch <tkirsch79@googlemail.com>
2018-09-17 21:55:22 +0200

    add Error component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p v-if="errors.length">
+    <p v-if="hasErrors">
       <b>Please correct the following error(s):</b>
       <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        <!-- <li v-for="error in errors" v-bind:key="error">{{ error }}</li> -->
+        <Error v-for="error in errors" v-bind:key="error">{{ error }}</Error>
       </ul>
     </p>
     <form v-on:submit.prevent="login">
       <input type="text" v-model="user.username"><br>
       <input type="password" v-model="user.password"><br>
-      <button type="submit">OK</button>
+      <button type="submit" v-on:click="disableSubmit" v-bind:disabled="submitDisabled">OK</button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: "http://localhost:8080/api"
 });
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    Error: {
+      name: "Error",
+      render(h) {
+        return h("li", this.$slots.default);
+      }
+    }
+  },
   props: {
     msg: String
   },
   data: function() {
     return {
       errors: [],
+      submitDisabled: false,
       user: {
         username: null,
         password: null,
         rememberMe: false
       }
+    };
+  },
+  computed: {
+    hasErrors() {
+      return this.errors.length;
     }
   },
   methods: {
-    login(){
-      this.errors=[];
-      API.post('/authenticate', this.user)
+    login() {
+      this.errors = [];
+      API.post("/authenticate", this.user)
         .then(response => {
           // JSON responses are automatically parsed.
-          this.response = response.data
-          console.log(response.data)
+          this.response = response.data;
+          console.log(response.data);
         })
         .catch(e => {
-          console.log(e)
-          this.errors.push(e)
+          console.log(e);
+          this.errors.push(e);
+          this.submitDisabled = false;
         });
+    },
+    disableSubmit() {
+      this.submitDisabled = true;
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
